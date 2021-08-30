f3fbc98eb4d9ace471c1de787466cc4a92ee134d
Dane Jorgensen <spam@danej.com>
2021-08-30 12:04:35 -0600

    testing exports

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import createAdminStore from './packages/ra-core/src/core/createAdminStore';
 import raDataJsonServer from './packages/ra-data-json-server/src';
-import useLogin from './packages/ra-core/src/auth/useLogin';
-import useLogout from './packages/ra-core/src/auth/useLogout';
-import * as fetchUtils from './packages/ra-core/src/dataProvider/fetch';
+//import useLogin from './packages/ra-core/src/auth/useLogin';
+//import useLogout from './packages/ra-core/src/auth/useLogout';
+//import * as fetchUtils from './packages/ra-core/src/dataProvider/fetch';
 
-export createAdminStore;
-export raDataJsonServer;
+export { createAdminStore };
+export { raDataJsonServer };
 
 /*
 export {
