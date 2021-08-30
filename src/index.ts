b11564cd25c0877893d59a57a04bdc9871740c47
Dane Jorgensen <spam@danej.com>
2021-08-30 12:19:22 -0600

    v0.0.6

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import createAdminStore from './packages/ra-core/src/core/createAdminStore';
+import raDataJsonServer from './packages/ra-data-json-server/src/index';
+import useLogin from './packages/ra-core/src/auth/useLogin';
+import useLogout from './packages/ra-core/src/auth/useLogout';
+import * as fetchUtils from './packages/ra-core/src/dataProvider/fetch';
+
+export {
+  createAdminStore,
+  fetchUtils,
+  raDataJsonServer,
+  useLogin,
+  useLogout,
+};
