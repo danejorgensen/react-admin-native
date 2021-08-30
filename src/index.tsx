b6f9cc0b37d6a5c3f04c34c60c4271b1c5db54e8
Dane Jorgensen <spam@danej.com>
2021-08-30 10:19:30 -0600

    add ra-data-json-server

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import createAdminStore from './packages/ra-core/src/core/createAdminStore';
+import raDataJsonServer from './packages/ra-data-json-server/src';
 import useLogin from './packages/ra-core/src/auth/useLogin';
 import useLogout from './packages/ra-core/src/auth/useLogout';
 import * as fetchUtils from './packages/ra-core/src/dataProvider/fetch';
@@ -6,6 +7,7 @@ import * as fetchUtils from './packages/ra-core/src/dataProvider/fetch';
 export {
   createAdminStore,
   fetchUtils,
+  raDataJsonServer,
   useLogin,
   useLogout,
 };
