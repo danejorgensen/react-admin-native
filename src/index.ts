280d0464e743c72dfda62705e2bcb9fbb0530699
Dane Jorgensen <spam@danej.com>
2021-11-27 12:36:48 -0700

    export util folder

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ export * from './packages/ra-core/src/auth';
 export * from './packages/ra-core/src/core';
 export * from './packages/ra-core/src/dataProvider';
 export * from './packages/ra-core/src/sideEffect';
+export * from './packages/ra-core/src/util';
 // export * from './packages/ra-core/src/types';
 
 export {
